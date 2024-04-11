6e8e057610e9834b6c7bce6049b5de74ab3d8627
Miguel Romero <mrm040@bucknell.edu>
2024-04-11 15:48:02 -0400

    movement and tile class created

---
+++ src/main/java/org/MMMJ/Movement.java
@@ -0,0 +1,46 @@
+/******************************************
+ *CSCI 205 - Software Engineering and Design
+ *Spring 2024
+ *Instructor: Prof. Lily Romano / Prof. Joshua Stough
+ *
+ *Name: Miguel Romero
+ *Section: 01
+ *Date: 4/11/24
+ *Time: 3:25 PM
+ *
+ *Project: csci205_final_project
+ *Package: org.MMMJ
+ *Class: Movement
+ *Description:
+ * **************************************** */
+package org.MMMJ;
+
+public class Movement {
+
+    /**
+     * Moving up
+     */
+    boolean up;
+    /**
+     * Moving down
+     */
+    boolean down;
+    /**
+     * moving left
+     */
+    boolean left;
+    /**
+     * moving right
+     */
+    boolean right;
+
+    public Movement() {
+
+    }
+
+    public void move(){
+
+    }
+
+
+}
---
+++ src/main/java/org/MMMJ/Tile.java
@@ -0,0 +1,60 @@
+/******************************************
+ *CSCI 205 - Software Engineering and Design
+ *Spring 2024
+ *Instructor: Prof. Lily Romano / Prof. Joshua Stough
+ *
+ *Name: Miguel Romero
+ *Section: 01
+ *Date: 4/11/24
+ *Time: 3:34 PM
+ *
+ *Project: csci205_final_project
+ *Package: org.MMMJ
+ *Class: Tile
+ *Description:
+ * **************************************** */
+package org.MMMJ;
+
+import javafx.scene.paint.Color;
+
+public class Tile {
+    private int currNum;
+
+    private Color currColor;
+
+    public Tile(){
+        this.currNum = 2;
+    }
+
+    public Tile(int tileNumber){
+        this.currNum = tileNumber;
+    }
+
+    /**
+     * Equals method which comes to true if the two Tile objects have the same current number
+     *
+     * @param tile2 the tile its being compared to
+     * @return whether it comes out to true
+     */
+    public boolean equals(Tile tile2) {
+        if (this.currNum == tile2.getCurrNum()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(currNum);
+    }
+
+    /**
+     * @return the tile objects current number
+     */
+    public int getCurrNum() {
+        return currNum;
+    }
+
+
+}
