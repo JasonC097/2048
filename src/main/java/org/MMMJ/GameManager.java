cf3ab3132e56baac7e5d0849c277af9467956b24
Jason Chung <jc097@bucknell.edu>
2024-04-21 14:46:40 -0400

    Jason- GameManager Unit tests done

--- src/main/java/org/MMMJ/GameManager.java
+++ src/main/java/org/MMMJ/GameManager.java
@@ -12,24 +12,32 @@
  * Package: org.MMMJ
  * Class: GameManager
  *
- * Description:
+ * Description: GameManager class for 2048
  *
  * ****************************************
  */
 package org.MMMJ;
 
+/**
+ * @author Jason Chung
+ * Class to handle win and lose conditions
+ */
 public class GameManager {
     /** The board of the game*/
     private Board board;
 
     private final int BOARD_SIZE = 4; // Can change later for making custom board sizes
 
-    private final int GAME_END_NUMBER = 32; // Change to 2048 for actual game, smaller number for testing only
+    private int gameEndNumber; // Change to 2048 for actual game, smaller number for testing only
 
     private Movement movement; // Sounds clunky, change later to maybe something abstract or interface
 
+    /**
+     * Constructor class for GameManager that generates the default board and movement
+     */
     public GameManager() {
         this.board = new Board(BOARD_SIZE);
+        this.gameEndNumber = 2048; //Default way for user to win
         this.movement = new Movement(this.board);
 
         // Probably make customization to let person play with smaller number if 2048 is too hard to achieve
@@ -38,16 +46,35 @@ public class GameManager {
     }
 
     /**
+     * Constructor for user that wants a different board size and different number to get to
+     * @param userDesiredSize - integer of the user's desired board size
+     * @param userDesiredEndNum - integer of the user's desired end number
+     */
+    public GameManager(int userDesiredSize, int userDesiredEndNum){
+        this.board = new Board(userDesiredSize);
+        this.gameEndNumber = userDesiredEndNum;
+        this.movement = new Movement(this.board);
+    }
+    /** Getter method for accessing the board for unit testing*/
+    public Board getBoard() {return board;}
+
+    /** Getter method for accessing movement for unit testing*/
+    public Movement getMovement() {return movement;}
+
+    /**
      * Player win is defined as player reaches the desired number
      * @return boolean true if player has reached the desired number. Otherwise, return false
      */
-    private boolean didPlayerWin() {
+    public boolean didPlayerWin() {
         // Block 2 is the new number to replace the combined numbers
         // Grab the value of the new number to see if it is the desired number to end the game
-        int checkedNewNum = this.movement.getCombining().getBlock2().getCurrNum();
-        if (checkedNewNum == GAME_END_NUMBER){
-            System.out.println("You win!");
-            return true;
+        for (Tile [] row : this.board.getBoard()){
+            for (Tile tile : row){
+                if (tile.getCurrNum() == this.gameEndNumber){
+                    System.out.println("You win!");
+                    return true;
+                }
+            }
         }
         return false;
     }
@@ -56,7 +83,7 @@ public class GameManager {
      * Player lose is defined as no more possible movements can be made
      * @return boolean true if player can not make more moves. Otherwise, return false if possible moves can still be made
      */
-    private boolean didPlayerLose() throws TileOccupiedException, OutOfBoardException {
+    public boolean didPlayerLose() throws TileOccupiedException, OutOfBoardException {
         // Create copy of board and movement to not interfere with user's current board
         // Used for predicting possible movements
         Board tempBoard = this.board;
@@ -74,6 +101,5 @@ public class GameManager {
             return false;
         }
     }
-
 
 }
---
+++ src/test/java/org/MMMJ/GameManagerTest.java
@@ -0,0 +1,59 @@
+package org.MMMJ;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Jason Chung
+ * Unit tests for GameManager class
+ */
+class GameManagerTest {
+
+    private GameManager game;
+    @BeforeEach
+    void setUp() throws TileOccupiedException, OutOfBoardException {
+        // Desired board and goal
+        this.game = new GameManager(3, 32);
+        // Fill board with tiles
+        this.game.getBoard().addTile(1,1, new Tile(16));
+        this.game.getBoard().addTile(0, 0, new Tile(2));
+        this.game.getBoard().addTile(0, 2, new Tile(3));
+        this.game.getBoard().addTile(2, 0, new Tile(1));
+        this.game.getBoard().addTile(2, 2, new Tile(5));
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void didPlayerWin () throws TileOccupiedException, OutOfBoardException {
+        // Make sure the game isn't automatically in win condition
+        assertFalse(this.game.didPlayerWin());
+        this.game.getBoard().addTile(0, 1, new Tile(16));
+        this.game.getMovement().moveTile("w");
+        // Should see that player has 32, which is the win condition
+        assertTrue(this.game.didPlayerWin());
+    }
+
+    @Test
+    void didPlayerLose() throws TileOccupiedException, OutOfBoardException {
+        // Make sure player isn't automatically in lose condition
+        assertFalse(this.game.didPlayerLose());
+        this.game.getBoard().addTile(0, 1, new Tile(6));
+        // Should still be possible to make moves
+        assertFalse(this.game.didPlayerLose());
+        this.game.getBoard().addTile(1, 0, new Tile(7));
+        // Should still be possible to make moves
+        assertFalse(this.game.didPlayerLose());
+        this.game.getBoard().addTile(1, 2, new Tile(8));
+        // Should still be possible to make moves
+        assertFalse(this.game.didPlayerLose());
+        this.game.getBoard().addTile(2,1,new Tile(9));
+        // Should now be true since no more moves possible
+        assertTrue(this.game.didPlayerLose());
+    }
+}
